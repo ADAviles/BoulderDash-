c1838559d3a945b7a08df58f7eef724aee26a158
Brendan Emmons <brendank12@yahoo.com>
2020-03-18 21:49:09 -0400

    Changed

---
+++ Assets/Scripts/control.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class control : MonoBehaviour
+{
+    public float Speed = 1f;
+    private Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    public void Movement()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * Speed);
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# BoulderDash-
-This is the Boulder Dash Repository for Team AA-R.O.B.
