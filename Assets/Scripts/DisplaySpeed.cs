f2197f23a1708473ba1b5ce1486c09e0181c4fae
Brendan Emmons <brendank12@yahoo.com>
2020-03-21 19:06:39 -0400

    added

---
+++ Assets/Scripts/DisplaySpeed.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplaySpeed : MonoBehaviour
+{
+    private float speed;
+
+    void FixedUpdated()
+    {
+        speed = GetComponent<Rigidbody>().velocity.magnitude;
+    }
+
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 100, 90), "Measurements");
+
+        GUI.Label(new Rect(20, 40, 80, 20), speed + "m/s");
+    }
+
+}
