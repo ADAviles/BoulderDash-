cff2303c3570d6ce8f543d8f8e492ce503beb431
Brendan Emmons <brendank12@yahoo.com>
2020-03-22 17:06:39 -0400

    fixing

---
+++ Assets/Scripts/QuitScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boulder"))
+        {
+            Application.Quit();
+        }
+        if (other.CompareTag("Rubber"))
+        {
+            Application.Quit();
+        }
+    }
+}
