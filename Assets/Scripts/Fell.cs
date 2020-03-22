3d0202950597f4c6a90e7e480c31f84dcb568e6f
Brendan Emmons <brendank12@yahoo.com>
2020-03-21 21:48:11 -0400

    kermit

--- Assets/Scripts/Fell.cs
+++ Assets/Scripts/Fell.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Fell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Vector3 place;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        place = new Vector3(0, 5, 0);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Boulder"))
+        {
+            other.transform.position = place;
+            other.attachedRigidbody.velocity = new Vector3(0,0,0);
+        }
+
+        if (other.gameObject.CompareTag("Rubber"))
+        {
+            other.transform.position = place;
+            other.attachedRigidbody.velocity = new Vector3(0, 0, 0);
+        }
     }
 }
---
+++ Assets/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boulder"))
+        {
+            SceneManager.LoadScene(2);
+        }
+        if (other.CompareTag("Rubber"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
