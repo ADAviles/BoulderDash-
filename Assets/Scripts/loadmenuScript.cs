b0708f4bc1f21d2a243b9641d756b51093fcae1f
Brendan Emmons <brendank12@yahoo.com>
2020-03-23 00:01:00 -0400

    fixing

--- Assets/Scripts/loadmenuScript.cs
+++ Assets/Scripts/loadmenuScript.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class loadmenuScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.CompareTag("Boulder"))
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
