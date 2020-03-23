17254177da9b6dc8ccf255ba3ec47e1370666ac0
Brendan Emmons <brendank12@yahoo.com>
2020-03-22 20:22:36 -0400

    Credits Implemented

--- Assets/Scripts/Credits.cs
+++ Assets/Scripts/Credits.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class Credits : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject credits;
+    void OnMouseDown()
     {
-        
+
+        credits.SetActive(true);
+
     }
 }
