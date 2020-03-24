2699b4bdb67f96007d313be2c0ab43066e396549
Brendan Emmons <brendank12@yahoo.com>
2020-03-24 06:59:48 -0400

    so much

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public DisplaySpeed DS;
+    public float RequiredSpeed;
+    public bool Speed1;
+    public bool Speed2;
+    public GameObject Together;
+    public GameObject Broken;
+
     void Start()
     {
         
@@ -13,6 +19,42 @@ public class Door : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        SpeedCheck();
+        BreakDoor();
+    }
+
+    public void SpeedCheck()
+    {
+        if (DS.speed >= RequiredSpeed)
+        {
+            Speed1 = true;
+        }
+        if (DS.speed < RequiredSpeed)
+        {
+            Speed1 = false;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boulder"))
+        {
+            Speed2 = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Boulder"))
+        {
+            Speed2 = false;
+        }
+    }
+
+    public void BreakDoor()
+    {
+        if (Speed1 == true && Speed2 == true)
+        {
+            Together.SetActive(false);
+            Broken.SetActive(true);
+        }
     }
 }
